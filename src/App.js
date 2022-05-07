95dc3dbbcda4b9084c370e8e16964f88025453c7
kaktusi112 <tevdoadze@gmail.com>
2022-05-07 11:45:32 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Buttons from './Components/Basic';
-import Check from './Components/List with description';
+import Category from './Components/Basic';
 
 function App() {
 
  return(
     <div className='w-screen h-screen flex justify-center items-center align-center'>
-     <div className='ml-auto'><Check/></div>
-     <div className='ml-auto mt-auto'><Buttons/></div>
+     <div className='m-auto'><Category/></div>
      </div>
  )
 }
--- src/Components/Basic.js
+++ src/Components/Basic.js
@@ -1,13 +1,41 @@
 /* This example requires Tailwind CSS v2.0+ */
-export default function Buttons() {
+export default function Category() {
   return (
     <span className="relative z-0 inline-flex shadow-sm rounded-md">
       <button
         type="button"
-        className="bg-blue-400 relative inline-flex items-center px-4 py-2 rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        onClick={ClickDaButton}
+        id='rame'
+        className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
       >
-        Next
+        Math Quiz
+      </button>
+      <button
+        type="button"
+        id='rame1'
+        onClick={ClickDaButton1}
+        className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+      >
+        History Quiz
+      </button>
+      <button
+        type="button"
+        id='rame2'
+        onClick={ClickDaButton2}
+        className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+      >
+        Sports Quiz
       </button>
     </span>
   )
+  function ClickDaButton(){
+    document.getElementById('rame').innerHTML = "Start Quiz";
+    
+  }
+  function ClickDaButton1(){
+    document.getElementById('rame1').innerHTML = "Start Quiz";
+  }
+  function ClickDaButton2(){
+    document.getElementById('rame2').innerHTML = "Start Quiz";
+  }
 }
